5a2c0854c8adc6f6e8ce8da38b7ba2352e554b6f
wali55 <walisantunu@gmail.com>
2023-05-13 13:29:15 +0600

    primary button

--- src/Button.jsx
+++ src/Button.jsx
@@ -11,7 +11,7 @@ function Button({
     rounded
 }) {
     return(
-        <button>{children}</button>
+        <button className="px-3 py-1.5 border border-blue-500 bg-blue-500 text-white">{children}</button>
     );
 }
 
