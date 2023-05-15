5ae739706a4f8e192af1cd08e607392651da3955
wali55 <walisantunu@gmail.com>
2023-05-15 21:56:33 +0600

    accordion

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
-import ButtonPage from './pages/ButtonPage';
+import Accordion from './components/Accordion';
 
 function App() {
+    const items = [
+        {
+            id: 'hek',
+            label: 'Can I use React on a project?',
+            content: 'You can use React any project you want. You can use React any project you want. You can use React any project you want.'
+        },
+        {
+            id: 'hjhs',
+            label: 'Can I use JavaScript on a project?',
+            content: 'You can use JavaScript any project you want. You can use JavaScript any project you want. You can use JavaScript any project you want.'
+        },
+        {
+            id: 'asfd',
+            label: 'Can I use CSS on a project?',
+            content: 'You can use CSS any project you want. You can use CSS any project you want.You can use CSS any project you want.'
+        }
+    ];
+
     return(
         <div>
-            <ButtonPage />
+            <Accordion items={items} />
         </div>
     );
 }
