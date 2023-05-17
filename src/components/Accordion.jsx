626eeb322b7428de54f065e2be1878e2ba86136c
wali55 <walisantunu@gmail.com>
2023-05-17 19:08:26 +0600

    accordion component

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -4,16 +4,12 @@ function Accordion({ items }) {
     const [expandedIndex, setExpandedIndex] = useState(0);
 
     const renderedItems = items.map((item, index) => {
-        if(index === expandedIndex) {
-            console.log('expanded!')
-        } else {
-            console.log('collapsed')
-        }
-
+        const isExpanded = index === expandedIndex;
+        
         return(
             <div key={item.id}>
                 <div>{item.label}</div>
-                <div>{item.content}</div>
+                {isExpanded && <div>{item.content}</div>}
             </div>
         );
     });
