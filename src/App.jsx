3128ffb0e9904d9400a19033b090ebf6857dc22f
wali55 <walisantunu@gmail.com>
2023-05-19 22:38:50 +0600

    dropdown functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
+import { useState } from 'react';
 import Dropdown from './components/Dropdown';
 
 function App() {
+    const [selection, setSelection] = useState(null);
+
+    const handleSelect = (option) => {
+        setSelection(option);
+    };
+
     const options = [
         { label: 'Red', value: 'red' },
         { label: 'Green', value: 'green' },
         { label: 'Blue', value: 'blue' },
-    ]
+    ];
 
     return(
         <div>
-            <Dropdown options={options} />
+            <Dropdown selection={selection} onSelect={handleSelect} options={options} />
         </div>
     );
 }
--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,7 +1,36 @@
-function Dropdown({ options }) {
+import { useState } from 'react';
+
+function Dropdown({ options, selection, onSelect }) {
+    const [isOpen, setIsOpen] = useState(false);
+    
+    const handleClick = () => {
+        setIsOpen(!isOpen);
+    };
+
+    const handleOptionClick = (option) => {
+        setIsOpen(false);
+        onSelect(option);
+    };
+
+    const renderedOptions = options.map((option) => {
+        return(
+            <div onClick={() => handleOptionClick(option)} key={option.value}>
+                {option.label}
+            </div>
+        );
+    }); 
+
+    let content = 'Select...';
+    if (selection) {
+        content = selection.label;
+    }
+
     return(
         <div>
-            Dropdown
+            <div onClick={handleClick}>
+                {content}
+            </div>
+            {isOpen && <div>{renderedOptions}</div>}
         </div>
     );
 }
