343e746a36719b99030e1725fea4b386a215d20d
wali55 <walisantunu@gmail.com>
2023-05-20 18:37:30 +0600

    create panel component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,4 +1,4 @@
-import className from 'classnames';
+import classNames from 'classnames';
 import { twMerge } from 'tailwind-merge';
 
 function Button({ 
@@ -12,7 +12,7 @@ function Button({
     rounded,
     ...rest
 }) {
-    let classes = className(rest.className, 'flex items-center px-3 py-1.5 border', {
+    let classes = classNames(rest.className, 'flex items-center px-3 py-1.5 border', {
         'border-blue-500 bg-blue-500 text-white': primary,
         'border-gray-900 bg-gray-900 text-white': secondary,
         'border-green-500 bg-green-500 text-white': success,
