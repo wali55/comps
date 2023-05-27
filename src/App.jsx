d03a5f2d15db06fa34b2cac09570e91a10a1f9e3
wali55 <walisantunu@gmail.com>
2023-05-27 15:18:32 +0600

    create NavigationContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,9 @@
-import { useState } from 'react';
-import Dropdown from './components/Dropdown';
 
 function App() {
-    const [selection, setSelection] = useState(null);
-
-    const handleSelect = (option) => {
-        setSelection(option);
-    };
-
-    const options = [
-        { label: 'Red', value: 'red' },
-        { label: 'Green', value: 'green' },
-        { label: 'Blue', value: 'blue' },
-    ];
-
+    
     return(
-        <div className="flex">
-            <a href="/dashboard">Dashboard</a>
-            <Dropdown value={selection} onChange={handleSelect} options={options} />
-            <Dropdown value={selection} onChange={handleSelect} options={options} />
+        <div>
+            
         </div>
     );
 }
