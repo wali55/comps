fd8bf62461c9cb2dc595e3697f10b59b71b4e4dc
wali55 <walisantunu@gmail.com>
2023-06-11 20:52:54 +0600

    Link styling

--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -4,7 +4,7 @@ import useNavigation from '../hooks/use-navigation';
 function Link({ to, children }) {
     const { navigate } = useNavigation();
 
-    const classes = classNames('text-blue-500');
+    const classes = classNames('text-blue-500', 'mb-3');
 
     const handleClick = (event) => {
         if (event.metaKey || event.ctrlKey) {
