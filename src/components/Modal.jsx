d45bc9ae403b269bd35a27ee5e0f95956f1620b1
wali55 <walisantunu@gmail.com>
2023-06-16 18:48:46 +0600

    modal styling

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,7 +1,8 @@
 function Modal() {
     return(
         <div>
-            Modal
+            <div className="absolute inset-0 bg-gray-300 opacity-80"></div>
+            <div className="absolute inset-40 p-10 bg-white">Modal</div>
         </div>
     );
 }
