391e65ec3d853312061efb0e037b2d2d63219c9f
wali55 <walisantunu@gmail.com>
2023-06-20 17:55:55 +0600

    customizing the modal

--- src/pages/ModalPage.jsx
+++ src/pages/ModalPage.jsx
@@ -13,10 +13,22 @@ function ModalPage() {
         setShowModal(false);
     };
 
+    const actionBar = (
+        <div>
+            <Button onClick={handleClose} primary>I Accept</Button>
+        </div>
+    );
+
+    const modal = (
+        <Modal onClose={handleClose} actionBar={actionBar}>
+            <p>Here is an important agreement for you to accept.</p>
+        </Modal>  
+    );
+
     return(
         <div className="relative">
             <Button onClick={handleClick} primary>Open Modal</Button>
-            {showModal && <Modal onClose={handleClose} />}
+            {showModal && modal}
         </div>
     );
 }
