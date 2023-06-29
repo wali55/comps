692dd80415eb10c0d93027d60057a7ed889872ee
wali55 <walisantunu@gmail.com>
2023-06-29 17:56:07 +0600

    dynamic table header

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,4 +1,10 @@
-function Table({ data }) {
+function Table({ data, config }) {
+    const renderedHeaders = config.map((column) => {
+        return(
+            <th key={column.label}>{column.label}</th>
+        );
+    });
+
     const renderedRows = data.map((fruit) => {
         return(
             <tr className="border-b" key={fruit.name}>
@@ -15,9 +21,7 @@ function Table({ data }) {
         <table className="table-auto border-spacing-2">
             <thead>
                 <tr className="border-b-2">
-                    <th>Fruits</th>
-                    <th>Color</th>
-                    <th>Score</th>
+                    {renderedHeaders}
                 </tr>
             </thead>
             <tbody>
--- src/pages/TablePage.jsx
+++ src/pages/TablePage.jsx
@@ -5,12 +5,18 @@ function TablePage() {
         { name: 'Orange', color: 'bg-orange-500', score: 5 },
         { name: 'Apple', color: 'bg-red-500', score: 3 },
         { name: 'Banana', color: 'bg-yellow-500', score: 1 },
-        { name: 'Lime', color: 'bg-green-500', score: 4 }
+        { name: 'Lime', color: 'bg-green-500', score: 4 },
+    ];
+
+    const config = [
+        { label: 'Fruits' },
+        { label: 'Color' },
+        { label: 'Score' },
     ];
 
     return(
         <div>
-            <Table data={data} />
+            <Table data={data} config={config} />
         </div>
     );
 }
