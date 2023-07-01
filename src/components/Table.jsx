bbfe200a61c90787bfa2657a50129de35a73c408
wali55 <walisantunu@gmail.com>
2023-07-01 17:08:44 +0600

    nested map

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -6,13 +6,15 @@ function Table({ data, config }) {
     });
 
     const renderedRows = data.map((fruit) => {
+        const renderedCells = config.map((column) => {
+            return(
+                <td className="p-4" key={column.label}>{column.render(fruit)}</td>
+            );
+        });
+
         return(
             <tr className="border-b" key={fruit.name}>
-                <td className="p-3">{config[0].render(fruit)}</td>
-                <td className="p-3">
-                    {config[1].render(fruit)}
-                </td>
-                <td className="p-3">{config[2].render(fruit)}</td>
+                {renderedCells}
             </tr>
         );
     });
