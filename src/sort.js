0d09ef7d282415e4b5d0d5bf46ca3483d2ae1aa1
wali55 <walisantunu@gmail.com>
2023-07-05 12:01:49 +0600

    sorting objects

--- src/sort.js
+++ src/sort.js
@@ -1,5 +1,20 @@
-const data = ['t', 'A', 'B', 'b', 'a'];
+const data = [
+    { name: 'Tomato', cost: 10, weight: 5 },
+    { name: 'Carrot', cost: 15, weight: 2 },
+    { name: 'Onion', cost: 5, weight: 7 },
+];
+
+function getSortValue(vegetable) {
+    return vegetable.name;
+}
 
 data.sort((a, b) => {
-    return a.localeCompare(b);
-});
+    const valueA = getSortValue(a);
+    const valueB = getSortValue(b);
+
+    if (typeof valueA === 'string') {
+        return valueA.localeCompare(valueB);
+    } else {
+        return valueA - valueB;
+    }
+}); 
