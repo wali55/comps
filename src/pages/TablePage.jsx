02bc350f4fb615783153acfb06e0b40ec781cf1b
wali55 <walisantunu@gmail.com>
2023-07-08 14:26:16 +0600

    customizing table header

--- src/pages/TablePage.jsx
+++ src/pages/TablePage.jsx
@@ -19,7 +19,8 @@ function TablePage() {
         },
         { 
             label: 'Score',
-            render: (fruit) => fruit.score
+            render: (fruit) => fruit.score,
+            header: () => <th className="bg-red-500">Score</th>
         },
     ];
 
