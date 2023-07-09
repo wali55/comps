514a7b7859762e41d75eb0d52e5e140ce115b14c
wali55 <walisantunu@gmail.com>
2023-07-09 13:41:00 +0600

    React Fragment

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,7 +1,11 @@
+import { Fragment } from 'react';
+
 function Table({ data, config, keyFn }) {
     const renderedHeaders = config.map((column) => {
         if (column.header) {
-            return column.header();
+            return(
+                <Fragment key={column.label}>{column.header()}</Fragment>
+            );
         }
         
         return(
