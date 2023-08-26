a40b456eca71ad82119567d2e3ea2426bbbb55f8
wali55 <walisantunu@gmail.com>
2023-08-26 22:30:40 +0600

    logic when user click header

--- src/components/SortableTable.jsx
+++ src/components/SortableTable.jsx
@@ -4,25 +4,43 @@ import Table from './Table';
 function SortableTable(props) {
   const [sortOrder, setSortOrder] = useState(null);
   const [sortBy, setSortBy] = useState(null);
-  
+
   const { config } = props;
 
   const handleClick = (label) => {
-    console.log(label)
-  }
+    if (sortOrder === null) {
+      setSortOrder('asc');
+      setSortBy(label);
+    } else if (sortOrder === 'asc') {
+      setSortOrder('desc');
+      setSortBy(label);
+    } else if (sortOrder === 'desc') {
+      setSortOrder(null);
+      setSortBy(null);
+    }
+  };
 
   const updatedConfig = config.map((column) => {
     if (!column.sortValue) {
-        return column;
+      return column;
     }
 
     return {
-        ...column,
-        header: () => <th onClick={() => handleClick(column.label)}>{column.label} is sortable</th>
-    }
-  })
+      ...column,
+      header: () => (
+        <th onClick={() => handleClick(column.label)}>
+          {column.label} is sortable
+        </th>
+      ),
+    };
+  });
 
-  return <Table {...props} config={updatedConfig} />;
+  return (
+    <div>
+      {sortOrder} - {sortBy}
+      <Table {...props} config={updatedConfig} />
+    </div>
+  );
 }
 
 export default SortableTable;
