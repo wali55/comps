328819059f4b0a158a382a855e3c38b3d6bf0982
wali55 <walisantunu@gmail.com>
2023-08-27 23:00:06 +0600

    data sorted

--- src/components/SortableTable.jsx
+++ src/components/SortableTable.jsx
@@ -5,7 +5,7 @@ function SortableTable(props) {
   const [sortOrder, setSortOrder] = useState(null);
   const [sortBy, setSortBy] = useState(null);
 
-  const { config } = props;
+  const { config, data } = props;
 
   const handleClick = (label) => {
     if (sortOrder === null) {
@@ -35,10 +35,31 @@ function SortableTable(props) {
     };
   });
 
+  // we need to sort data only when sortData && sortBy are not null
+  // make a copy of the 'data' prop
+  // find the correct sortValue function and use it for sorting
+
+  let sortedData = data;
+
+  if (sortOrder && sortBy) {
+    const { sortValue } = config.find((column) => column.label === sortBy);
+    sortedData = [...data].sort((a, b) => {
+      const valueA = sortValue(a);
+      const valueB = sortValue(b);
+
+      const reverseOrder = sortOrder === 'asc' ? 1 : -1;
+      if (typeof valueA === 'string') {
+        return valueA.localeCompare(valueB) * reverseOrder;
+      } else {
+        return (valueA - valueB) * reverseOrder;
+      }
+    });
+  }
+
   return (
     <div>
       {sortOrder} - {sortBy}
-      <Table {...props} config={updatedConfig} />
+      <Table {...props} data={sortedData} config={updatedConfig} />
     </div>
   );
 }
