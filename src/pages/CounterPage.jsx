5011008f1afccdad73888df8772b4a1c933ca1fe
wali55 <walisantunu@gmail.com>
2023-08-30 21:44:42 +0600

    counter component

---
+++ src/pages/CounterPage.jsx
@@ -0,0 +1,22 @@
+import { useState, useEffect } from 'react';
+import Button from '../components/Button';
+
+const CounterPage = ({ initialCount }) => {
+  const [count, setCount] = useState(initialCount);
+
+  useEffect(() => {
+    console.log(count);
+  }, [count]);
+
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <div>
+      <h1>Count is: {count}</h1>
+      <Button onClick={handleClick}>Increment</Button>
+    </div>
+  );
+};
+export default CounterPage;
